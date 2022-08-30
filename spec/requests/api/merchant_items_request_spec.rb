c713429bb9035ae1b3df359a74f080bae1657c51
Gauri Joshi <gauri728@gmail.com>
2022-08-30 12:00:37 -0700

    Merchant Items GET request

---
+++ spec/requests/api/merchant_items_request_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+  describe "MerchantItems API" do 
+    it "gets a merchant's items" do 
+
+    id = create(:merchant).id 
+    merchant_items = create_list(:item, 3, merchant_id: id) #faker lets us do this instead of manually creating merchant and items in test
+
+    get "/api/v1/merchants/#{id}/items"
+
+    merchant_items = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+    expect(merchant_items.count).to eq(3)
+
+    merchant_items.each do |merchant_item|
+      expect(merchant_item).to have_key(:id)
+      expect(merchant_item[:id]).to be_a(String)
+    end
+  end
+end
