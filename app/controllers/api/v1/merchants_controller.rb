b514027cfbdc7632cbb93e5d9c1e5856e83afedc
Gauri Joshi <gauri728@gmail.com>
2022-08-30 10:19:40 -0700

    tests merchant serializer for retrieving merchant by an id

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::MerchantsController < ApplicationController 
   def index 
     merchants = Merchant.all 
-    render json: MerchantSerializer.new(merchants)
+    render json: MerchantSerializer.new(merchants) #render this way when using serializer gem
+
+    #if using handrolling it would be:
+    #render json: MerchantSerializer.format_merchants(Merchant.all)
+  end
+
+  def show 
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
   end
 end
