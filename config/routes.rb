37d3c0694d4d7a26fc76d544b4710537870666a0
Gauri Joshi <gauri728@gmail.com>
2022-08-30 13:13:34 -0700

    GET all items endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do 
     namespace :v1 do 
-      resources :merchants, only: [:index, :show]
+      resources :items, only: [:index]
+      resources :merchants, only: [:index, :show] do 
+        resources :items, controller: 'merchant_items'
+      end
     end
   end
 end
