91009f2328c4ac35ce33b1a0ceedc92dd965b2b7
Gauri Joshi <gauri728@gmail.com>
2022-08-31 18:12:45 -0700

    test: sad path for finding merchant by name fragment when such merchant does not exist

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -24,6 +24,15 @@ class Api::V1::ItemsController < ApplicationController
     # item = Item.destroy(params[:id])
   end
 
+  # def find_price 
+  #   items = Item.where("unit_price = ?", params[:unit_price]).all
+  #   if items.nil?
+  #     render json: {data: {message: "No items found"}}
+  #   else
+  #     render json: ItemSerializer.new(items)
+  #   end
+  # end
+
   def find_all 
     items = Item.where("name ILIKE ?", "%#{params[:name]}%").all
     if items.nil?
